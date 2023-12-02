9f8b23ef895a2b421a097816f1d0b75a51aaa2d3
Charles Olupot <olupotcharles@gmail.com>
2023-12-02 14:32:16 +0300

    Some updates

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const fs = require("fs");
+const axios = require("axios/dist/node/axios.cjs");
 const args = require("minimist")(process.argv.slice(2));
 const { chunk } = require("lodash");
 
-const readJsonFile = (file) => {
-    const rawData = fs.readFileSync(file);
+const readJsonFile = async () => {
+    const api = axios.create({
+        baseURL: `${args.l}/api/`,
+        auth: {
+            username: args.u,
+            password: args.p,
+        },
+    });
+    const rawData = fs.readFileSync(args.f);
     const { trackedEntityInstances } = JSON.parse(rawData);
-    for (const c of chunk(trackedEntityInstances, 50)) {
-        console.log(c);
+    const allChunks = chunk(trackedEntityInstances, 50);
+    let index = 0;
+    for (const c of allChunks) {
+        console.log(`Working on ${++index} of ${allChunks.length}`);
+        await api.post("trackedEntityInstances", { trackedEntityInstances: c });
     }
 };
 
-readJsonFile(args.n);
+readJsonFile();
